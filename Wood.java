4168a729049049f19de28e8c4adc7292f8223d3e
Steel Legionnaire <theros113@gmail.com>
2025-09-11 10:25:40 -0400

    Wood will burn and has a chance to turn to ash

--- Wood.java
+++ Wood.java
@@ -3,8 +3,14 @@ public class Wood extends Tile{
 
     public boolean onFire = false;
 
+    // when Wood is on fire, this variable is how long it'll be on fire before it burns and potentially turns to ash
+    int framesUntilCanBeBurnt = 300;
+    int framesOnFire = 0;
+
+
+
     public Wood(int x, int y) {
-        super(x, y, new java.awt.Color(139,69,19), true, true, 4, 3);
+        super(x, y, new java.awt.Color(139,69,19), true, true, 4, 50);
     }
 
     @Override
@@ -13,7 +19,62 @@ public class Wood extends Tile{
     @Override
     public void action() 
     {
-        System.out.println(onFire);
+        if(onFire)
+        {
+            if(framesSinceLastUpdate == speed)
+            {
+                //System.out.println("is on fire");
+                Tile[][] grid = ParticulateGame.grid;
+
+                int chanceToGoOut = (int)(Math.random() *100) + 1;
+                //System.out.println("chanceToGoOut: "+chanceToGoOut);
+
+                if(chanceToGoOut <= 25)
+                {
+                    //System.out.println("TEST");
+                    onFire = false;
+                    framesOnFire = 0;
+                }
+                else
+                {
+                    if(framesOnFire >= framesUntilCanBeBurnt)
+                    {
+                        
+                        int chanceToBurn = (int)(Math.random() *100) + 1;
+                        //System.out.println(chanceToBurn);
+                        if(chanceToBurn <= 50)
+                        {
+                            int chanceToSpawnAsh = (int)(Math.random() *100) + 1;
+                            //System.out.println(chanceToSpawnAsh);
+                            if(chanceToSpawnAsh <= 80)
+                            {
+                                grid[y][x] = new Ash(x, y);
+                            }
+                            else 
+                            {
+                                grid[y][x] = null;
+                            }
+                        }
+
+
+
+                    }
+                }
+                framesSinceLastUpdate = 0;
+            }
+            framesSinceLastUpdate++;
+            framesOnFire++;
+            
+        }
+        else
+        {
+            //System.out.println(framesSinceLastUpdate);
+            if(framesSinceLastUpdate < speed)
+            {
+                framesSinceLastUpdate++;
+            }
+        }
     }
     
 }
+ 
