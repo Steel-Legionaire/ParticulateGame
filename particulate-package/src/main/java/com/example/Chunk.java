1556686ce4bc0ecf4dfb430da6d261f836c2ba11
Vince <theros113@gmail.com>
2025-09-25 10:05:44 -0400

    Fix chunk class

---
+++ particulate-package/src/main/java/com/example/Chunk.java
@@ -0,0 +1,63 @@
+package com.example;
+
+public class Chunk 
+{
+
+    // This class takes inspiration from DavidMcLaughlin208's GitHub page 
+    // where he also uses a chunk based system in his FallingSandGame
+    // Github link: https://github.com/DavidMcLaughlin208/FallingSandJava/blob/master/core/src/com/gdx/cellular/util/Chunk.java
+
+    public static int size = 32;
+    
+    private boolean shouldStep = true;
+    private boolean shouldStepNextFrame = true;
+
+    private int topLeft;
+    private int bottomRight;
+
+    public Chunk(int topLleft, int bottomRight)
+    {
+        this.topLeft = topLleft;
+        this.bottomRight = bottomRight;
+    }
+
+    public void setTopLeft(int topLeft)
+    {
+        this.topLeft = topLeft;
+    }
+    public int getTopLeft()
+    {
+        return this.topLeft;
+    }
+    
+    public void setBottomRight(int bottomRight)
+    {
+        this.bottomRight = bottomRight;
+    }
+    public int getBottomRight()
+    {
+        return this.bottomRight ;
+    }
+
+    public void setShouldStep(boolean shouldStep) {
+        this.shouldStep = shouldStep;
+    }
+
+    public boolean getShouldStep() {
+        return this.shouldStep;
+    }
+
+    public void setShouldStepNextFrame(boolean shouldStepNextFrame) {
+        this.shouldStepNextFrame = shouldStepNextFrame;
+    }
+
+    public boolean getShouldStepNextFrame() {
+        return this.shouldStepNextFrame;
+    }
+
+    public void shiftShouldStepAndReset() {
+        this.shouldStep = this.shouldStepNextFrame;
+        this.shouldStepNextFrame = false;
+    }
+
+}
