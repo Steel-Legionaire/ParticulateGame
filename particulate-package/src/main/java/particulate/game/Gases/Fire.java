c935b75123c7c69f44bb58fa1f16ef1665b71f97
Steel Legionnaire <162994122+Steel-Legionaire@users.noreply.github.com>
2025-09-30 19:48:45 -0400

    Made Fire inherit from Gases class,

--- particulate-package/src/main/java/particulate/game/Gases/Fire.java
+++ particulate-package/src/main/java/particulate/game/Gases/Fire.java
@@ -2,14 +2,13 @@ package particulate.game.Gases;
 
 import java.awt.Color;
 
+import particulate.game.CellularMatrix;
 import particulate.game.ParticulateGame;
 import particulate.game.Tile;
 import particulate.game.Solids.StaticSolids.TNT;
 import particulate.game.Solids.StaticSolids.Wood;
 
-public class Fire extends Tile{
-
-    private int lifeTime = (int)(Math.random()*20) + 50;
+public class Fire extends Gases{
 
     private static final Color[] COLORS = new Color[]{ new Color(255,135,0), new Color(255,142,0), new Color(255,150,0), new Color(255,158,0), new Color(255,165,0) };
     
@@ -17,123 +16,61 @@ public class Fire extends Tile{
         super(x, y, false, true, 1, 2);
         setAllPossibleColors(COLORS);
         setColor();
-    }
 
-    @Override
-    public void move() 
-    {
-        /*Tile[][] grid = ParticulateGame.grid;
-
-
-
-        if(framesSinceLastUpdate == speed)
-        {
-            // 0 - up
-            // 1 - right
-            // 2 - left
-            if(lifeTime == 0)
-            {
-                grid[y][x] = null;
-                return;
-            }
-
-            int randDir = (int)(Math.random() * 3);
-            //System.out.println(randDir);
-            if(randDir == 0)
-            {
-                if(grid[y-1][x] == null)
-                {
-                    grid[y][x] = null;
-                    y--;
-                    grid[y][x] = this;
-                    
-                }
-            }
-            else if(randDir == 1)
-            {
-                if(grid[y-1][x+1] == null && grid[y][x+1] == null)
-                {
-                    grid[y][x] = null;
-                    y--;
-                    x++;
-                    grid[y][x] = this;
-                }
-            }
-            else if(randDir == 2)
-            {
-                if(grid[y-1][x-1] == null && grid[y][x-1] == null)
-                {
-                    grid[y][x] = null;
-                    y--;
-                    x--;
-                    grid[y][x] = this;
-                }
-            }
-
-            Tile topTile = grid[y-1][x];
-            Tile rightTile = grid[y][x+1];
-            Tile leftTile = grid[y][x-1];
-
-
-            Tile bottomTile = null;
-
-            if(y+1 < grid.length)
-            {
-                bottomTile = grid[y+1][x];
-            }
-
-
-            if(topTile instanceof Wood)
-            {
-                ((Wood)grid[y-1][x]).onFire = true;
-            }
-            else if(topTile instanceof TNT)
-            {
-                ((TNT)topTile).explode();
-            }
-
-            if(rightTile instanceof Wood)
-            {
-                ((Wood)grid[y][x+1]).onFire = true;
-            }
-            else if(rightTile instanceof TNT)
-            {
-                ((TNT)rightTile).explode();
-            }
-
-            if(y+1 < grid.length &&  bottomTile instanceof Wood)
-            {
-                ((Wood)grid[y+1][x]).onFire = true;
-            }
-            else if(y+1 < grid.length && bottomTile instanceof TNT)
-            {
-                ((TNT)bottomTile).explode();
-            }
-
-            if(leftTile instanceof Wood)
-            {
-                ((Wood)grid[y][x-1]).onFire = true;
-            }
-            else if(leftTile instanceof TNT)
-            {
-                ((TNT)leftTile).explode();
-            }
-
-            lifeTime--;
-            framesSinceLastUpdate = 0; 
-        }
-        else
-        {
-            framesSinceLastUpdate++;
-        }*/
-
-
+        setLifeTime((int)(Math.random()*20) + 50);
     }
 
     @Override
     public void action() 
     {
 
+        CellularMatrix matrix = ParticulateGame.getMatrix();
+
+        Tile topTile = matrix.getTile(x,y-1);
+        Tile rightTile = matrix.getTile(x+1,y);
+        Tile leftTile = matrix.getTile(x-1,y);
+
+        Tile bottomTile = null;
+        if(matrix.withinBounds(x, y+1))
+        {
+            bottomTile = matrix.getTile(x,y+1);
+        }
+
+        if(topTile instanceof Wood)
+        {
+            ((Wood)topTile).onFire = true;
+        }
+        else if(topTile instanceof TNT)
+        {
+            ((TNT)topTile).explode();
+        }
+
+        if(rightTile instanceof Wood)
+        {
+            ((Wood)rightTile).onFire = true;
+        }
+        else if(rightTile instanceof TNT)
+        {
+            ((TNT)rightTile).explode();
+        }
+
+        if(bottomTile instanceof Wood)
+        {
+            ((Wood)bottomTile).onFire = true;
+        }
+        else if(bottomTile instanceof TNT)
+        {
+            ((TNT)bottomTile).explode();
+        }
+
+        if(leftTile instanceof Wood)
+        {
+            ((Wood)leftTile).onFire = true;
+        }
+        else if(leftTile instanceof TNT)
+        {
+            ((TNT)leftTile).explode();
+        }
     }
     
 }
