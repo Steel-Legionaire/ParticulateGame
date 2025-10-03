3805d58d59e8c5776920bed5aca01628b6bfd58d
Steel Legionnaire <162994122+Steel-Legionaire@users.noreply.github.com>
2025-10-02 21:51:44 -0400

    Update sand with new colors to look more like sand

--- particulate-package/src/main/java/particulate/game/Solids/MoveableSolids/Sand.java
+++ particulate-package/src/main/java/particulate/game/Solids/MoveableSolids/Sand.java
@@ -2,9 +2,7 @@ package particulate.game.Solids.MoveableSolids;
 import java.awt.Color;
 
 public class Sand extends MoveableSolid{
-
-    private static final Color[] COLORS = new Color[]{new Color(210, 190, 10), new Color(220, 200, 20), new Color(230, 210, 30), new Color(240, 220, 40), new Color(245, 225, 50)};
-
+    private static final Color[] COLORS = new Color[]{new Color(219, 212, 157), new Color(199, 192, 137), new Color(222, 215, 161), new Color(204, 199, 143), new Color(233, 223, 174)};
     public Sand(int x, int y) {
         super(x, y, false, true, 2,1);
 
